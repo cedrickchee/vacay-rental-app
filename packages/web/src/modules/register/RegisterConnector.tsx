baa884c08416ee3065aa03a1c036a52ee16d8d81
Cedric Chee <cedric@invictusbyte.com>
2018-08-20 19:07:01 +0800

    Basic React login form with Formik and Ant Design

--- packages/web/src/modules/register/RegisterConnector.tsx
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -1,3 +1,19 @@
 import * as React from "react";
+import { RegisterView } from "./ui/RegisterView";
 
-export const RegisterConnector = () => <div>register page</div>;
+// container -> view (presentational)
+// container -> connector -> view (presentational)
+
+// Naming convention
+// controller -> connector -> view
+
+export class RegisterConnector extends React.PureComponent {
+  dummySubmit = async (values: any) => {
+    console.log(values);
+    return null;
+  };
+
+  render() {
+    return <RegisterView submit={this.dummySubmit} />;
+  }
+}
