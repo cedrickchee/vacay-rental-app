e0dd98733fbb85228446c133d3b8cfa27b64cdd7
Cedric Chee <cedric@invictusbyte.com>
2018-08-23 14:09:09 +0800

    Deploy TypeScript server to Heroku with Docker container

--- deploy_server_aws.sh
+++ deploy_server_aws.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
+
+# Build server
 yarn build:server
-docker build -t cedrickchee/vacay-rental-app:latest .
-docker push cedrickchee/vacay-rental-app:latest
-ssh -i /media/cedric/Cypher/aws/invictus9_hydraops/rsa_keypair_dokku_dev.pem ubuntu@52.42.6.202 "sudo docker pull cedrickchee/vacay-rental-app:latest && sudo docker tag cedrickchee/vacay-rental-app:latest dokku/vacay:latest && dokku tags:deploy vacay latest"
+
+# Deploy to Heroku
+heroku container:push web
+heroku container:release web
+
+# Deploy to Dokku on AWS
+# docker build -t cedrickchee/vacay-rental-app:latest .
+# docker push cedrickchee/vacay-rental-app:latest
+# ssh -i /media/cedric/Cypher/aws/invictus9_hydraops/rsa_keypair_dokku_dev.pem ubuntu@52.42.6.202 "sudo docker pull cedrickchee/vacay-rental-app:latest && sudo docker tag cedrickchee/vacay-rental-app:latest dokku/vacay:latest && dokku tags:deploy vacay latest"
